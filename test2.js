41bb70c85c0b5fd9d2b624172ffb97d759f8b5d1
Victor Gavrya <vg@finch-melrose.com>
2015-11-05 17:44:08 +0300

    testing service workers

--- test2.js
+++ test2.js
@@ -1,5 +1,6 @@
 self.addEventListener('push', function(event) {  
   console.log('Received a push message', event);
+  console.log("event data", event.data);
 
   var title = 'Yay a message.';  
   var body = 'We have received a push message.';  
