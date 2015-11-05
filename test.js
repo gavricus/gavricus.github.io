7c86706d3e623cfb498fbf3bdd042703efbf3beb
Victor Gavrya <vg@finch-melrose.com>
2015-11-05 15:31:46 +0300

    testing service workers

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log(1);
+self.addEventListener('push', function(event) {  
+  console.log('Received a push message', event);
+
+  var title = 'Yay a message.';  
+  var body = 'We have received a push message.';  
+  var icon = '/images/icon-192x192.png';  
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+      body: body,  
+      icon: icon,  
+      tag: tag  
+    })  
+  );  
+});
